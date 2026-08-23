85717db4de40f44a33eb9f53206cbf7ea95803c2
gakimr <kimuragabriela.ramos@gmail.com>
2026-08-22 22:21:36 -0300

    ajuste no app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import Header from './components/Header/Header'
 import Home from './pages/Home/Home'
 import Footer from './components/Footer/Footer'
-import Mapa from './pages/App/Mapa/Mapa'
 import './App.css'
 
 function App() {
   return (
     <div>
-      <Header/>
-     <Mapa/>
+      <Header />
+      <Home />
+      <Footer/>
     </div>
   )
 }
