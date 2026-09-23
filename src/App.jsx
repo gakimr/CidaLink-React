5e7a8f9540987c89df8a663ea777899e77d189e3
gakimr <kimuragabriela.ramos@gmail.com>
2026-09-22 22:05:22 -0300

    criar ocorrencias

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import CadastroAdmin from "./pages/Login/CadastroAdmin";
 import AppLayout from "./layout/Applayout";
 import Mapa from "./pages/App/Mapa/Mapa";
 import Feed from "./pages/App/Feed/Feed";
+import CriarOcorrencia from "./pages/App/CriarOcorrencia/CriarOcorrencia";
 import "./App.css";
 
 function App() {
@@ -32,7 +33,8 @@ function App() {
 
       <Route element={<AppLayout />}>
         <Route path="/app/mapa" element={<Mapa />} />
-       <Route path="/app" element={<Feed />} />
+        <Route path="/app" element={<Feed />} />
+        <Route path="/app/criar-ocorrencia" element={<CriarOcorrencia />} />
       </Route>
     </Routes>
   );
